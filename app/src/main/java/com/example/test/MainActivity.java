2178ba19172706966683fad4a8079e157bb5ee56
chengzhiyaun163 <18718747876@163.com>
2020-08-05 14:31:45 +0800

    test success

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("kkkkkkkkk");
+        System.out.println("test success");
     }
 }
